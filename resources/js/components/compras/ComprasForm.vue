bf12051c30c05b7080254fbde422b95bcc3098d6
KazmerMaximiliano <kazmermaximiliano@gmail.com>
2020-02-21 18:13:07 -0300

    nothing

--- resources/js/components/compras/ComprasForm.vue
+++ resources/js/components/compras/ComprasForm.vue
@@ -1,33 +1,6 @@
 <template>
     <div>
-        <!-- HEADER -->
-        <div @click="focus = 'header'">
-            <v-card
-                shaped
-                outlined
-                class="form-header"
-                :class="focus == 'header' ? 'elevation-3' : ''"
-                :loading="inProcess"
-            >
-                <v-card-title class="py-0 px-2">
-                    <v-row class="pa-0 ma-0">
-                        <v-col cols="auto" align-self="center">Nueva Compra</v-col>
-                        <v-spacer></v-spacer>
-                        <v-col cols="auto">
-                            <v-list-item two-line class="text-right">
-                                <v-list-item-content>
-                                    <v-list-item-subtitle>
-                                        <b v-if="$vuetify.breakpoint.xsOnly">P:&nbsp;</b>
-                                        <b v-else>Punto de venta:&nbsp;</b>
-                                        {{ PuntoVenta }}
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                    </v-row>
-                </v-card-title>
-            </v-card>
-        </div>
+        <h1>Hola mundo</h1>
     </div>
 </template>
 
@@ -93,5 +66,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
