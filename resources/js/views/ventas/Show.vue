fdabce60ce3f2bd9d0aa6ee85cf18bbe28143237
germanio7 <german.e.escobar@gmail.com>
2020-05-11 14:52:32 -0300

    mi cuenta router go back

--- resources/js/views/ventas/Show.vue
+++ resources/js/views/ventas/Show.vue
@@ -2,7 +2,17 @@
     <div>
         <v-tooltip left>
             <template v-slot:activator="{ on }">
-                <v-btn color="secondary" dark fab fixed right bottom large v-on="on" to="/ventas">
+                <v-btn
+                    color="secondary"
+                    dark
+                    fab
+                    fixed
+                    right
+                    bottom
+                    large
+                    v-on="on"
+                    @click="$router.go(-1)"
+                >
                     <v-icon>fas fa-chevron-left</v-icon>
                 </v-btn>
             </template>
