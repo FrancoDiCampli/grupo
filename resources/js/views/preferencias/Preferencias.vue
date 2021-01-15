fb205c3981c82177f61d7c1cad5f5771e38a8cf2
KazmerMaximiliano <kazmermaximiliano@gmail.com>
2021-01-15 15:31:03 -0300

    new preferences

--- resources/js/views/preferencias/Preferencias.vue
+++ resources/js/views/preferencias/Preferencias.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <v-container>
+           
             <v-row justify="center">
-                <v-col cols="12" md="5">
+                <v-col cols="12" md="10" lg="8">
+                    <p class="title">Preferencias</p>
+                    <p class="subtitle">Colores</p>
                     <v-card outlined>
                         <v-card-text class="pa-0">
                             <v-expansion-panels accordion>
@@ -21,7 +24,10 @@
                                         {{ color.name }}
                                     </v-expansion-panel-header>
                                     <v-expansion-panel-content>
-                                        <v-row justify="center">
+                                        <div class="my-6">
+                                            {{ color.description }}
+                                        </div>
+                                        <v-row justify="center">                                            
                                             <v-color-picker
                                                 v-model="selectedColor.color"
                                                 mode="hexa"
@@ -33,102 +39,6 @@
                                     </v-expansion-panel-content>
                                 </v-expansion-panel>
                             </v-expansion-panels>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-                <v-col cols="12" md="5">
-                    <v-card outlined>
-                        <v-card-text class="pa-0">
-                            <v-toolbar color="primary" dark class="elevation-0">
-                                <v-toolbar-title>Grupo APC</v-toolbar-title>
-                            </v-toolbar>
-                            <br />
-                            <v-row justify="center">
-                                <v-col cols="10">
-                                    <v-card shaped outlined>
-                                        <v-card-text>
-                                            <v-simple-table>
-                                                <template v-slot:default>
-                                                    <thead>
-                                                        <tr>
-                                                            <th
-                                                                class="text-left"
-                                                            >
-                                                                Nombre
-                                                            </th>
-                                                            <th
-                                                                class="text-left"
-                                                            >
-                                                                Apellido
-                                                            </th>
-                                                            <th
-                                                                class="text-left"
-                                                            ></th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr
-                                                            v-for="(item,
-                                                            i) in 3"
-                                                            :key="i"
-                                                        >
-                                                            <td>
-                                                                Nombre
-                                                                {{ item }}
-                                                            </td>
-                                                            <td>
-                                                                Apellido
-                                                                {{ item }}
-                                                            </td>
-                                                            <td>
-                                                                <v-btn icon
-                                                                    ><v-icon
-                                                                        size="medium"
-                                                                        color="secondary"
-                                                                        >fas
-                                                                        fa-ellipsis-v</v-icon
-                                                                    ></v-btn
-                                                                >
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </template>
-                                            </v-simple-table>
-                                        </v-card-text>
-                                    </v-card>
-                                    <br />
-                                    <v-alert
-                                        color="error"
-                                        border="left"
-                                        colored-border
-                                        class="alert-border"
-                                        style="border: thin solid #e0e0e0; border-left: none;"
-                                    >
-                                        <div class="ma-2">
-                                            <h3>Grupo APC</h3>
-                                            <p class="mt-2 mb-4">
-                                                Grupo apc grupo apc grupo apc
-                                            </p>
-                                        </div>
-                                        <v-row justify="end" class="my-2">
-                                            <v-btn
-                                                tile
-                                                color="error"
-                                                dark
-                                                class="mx-2 elevation-0"
-                                                >Cancelar</v-btn
-                                            >
-                                            <v-btn
-                                                tile
-                                                color="success"
-                                                dark
-                                                class="mx-2 elevation-0"
-                                                >Aceptar</v-btn
-                                            >
-                                        </v-row>
-                                    </v-alert>
-                                </v-col>
-                            </v-row>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -145,23 +55,28 @@ export default {
             light: [
                 {
                     name: 'primary',
-                    color: '#44C2F7'
+                    color: '#44C2F7',
+                    description: 'El color “primario” (Primary), es usado en los elementos no interactuables del sistema y para indicar ciertos elementos activos, por ejemplo, los elementos del menu lateral.'
                 },
                 {
                     name: 'secondary',
-                    color: '#8DC638'
+                    color: '#8DC638',
+                    description: 'El color “secundario” (Secondary), es usado para remarcar los elementos interactuables del sistemas, tales como botones, menus, submenus, etc.'
                 },
                 {
                     name: 'error',
-                    color: '#F44336'
+                    color: '#F44336',
+                    description: 'El color “error”, no solo es usado en elementos que indican errores en el sistema, sino también en aquellos que advierten al usuario sobre la implicaciones de sus acciones.'
                 },
                 {
                     name: 'success',
-                    color: '#4CAF50'
+                    color: '#4CAF50',
+                    description: 'El color “éxito” (Success), es usado en elementos que indican que una operación fue realizada de forma exitosa o que hay una acción habilitada para dicho elemento.'
                 },
                 {
                     name: 'warning',
-                    color: '#FFC107'
+                    color: '#FFC107',
+                    description: 'El color “advertencia” (warning), es usado, en menor medida, junto con el color “error” para advertir al usuario sobre sus futuras acciones y para indicar irregularidades en ciertos elementos.'
                 }
             ]
         },
